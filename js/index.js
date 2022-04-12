5f199df107f48909358fd0edd2ef4733d9e5935c
Jessica-s-paiva <jessicapaivamvp@gmail.com>
2022-04-12 08:17:02 -0300

    adicionando evento de clique no botão enviar

--- js/index.js
+++ js/index.js
@@ -1,5 +1,19 @@
-function enviar() {
-    const data = $('#data');
-    const enviar = $('#enviar');
+const enviar = $('#enviar');
+enviar.click(function(event) {
+    event.preventDefault();
+    const data = $('#data').val();
     const info = $('#info');
-}
+    
+    const requisicao = new XMLHttpRequest();
+    requisicao.open('GET', `https://api.nasa.gov/planetary/apod?api_key=w1d9wvAkhHPGhaGl6WqpJYk444B9CYxJWiaBEPlZ&date=${data}&concept_tags=True`);
+    
+    requisicao.onload = function(){
+        if(requisicao.status == 200){
+            const req = JSON.parse(requisicao.response);
+            const data = req.date;
+            console.log(req);
+            info.innerText = data
+        }
+    }
+    requisicao.send();
+})
