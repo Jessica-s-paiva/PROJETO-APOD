685f5975be7a390541a2c942e465d1cba0fd67f4
Jessica-s-paiva <jessicapaivamvp@gmail.com>
2022-04-13 16:06:04 -0300

    arrumei a requisição e agora ja funciona a api

--- js/index.js
+++ js/index.js
@@ -1,19 +1,16 @@
-const enviar = $('#enviar');
-enviar.click(function(event) {
+$("#enviar").click(function(event){
     event.preventDefault();
-    const data = $('#data').val();
-    const info = $('#info');
-    
-    const requisicao = new XMLHttpRequest();
-    requisicao.open('GET', `https://api.nasa.gov/planetary/apod?api_key=w1d9wvAkhHPGhaGl6WqpJYk444B9CYxJWiaBEPlZ&date=${data}&concept_tags=True`);
-    
-    requisicao.onload = function(){
-        if(requisicao.status == 200){
-            const req = JSON.parse(requisicao.response);
-            const data = req.date;
-            console.log(req);
-            info.innerText = data
+    const data = $("#data").val();
+    console.log(data);
+
+    $.ajax({
+        url: `https://api.nasa.gov/planetary/apod?date=${data}&api_key=w1d9wvAkhHPGhaGl6WqpJYk444B9CYxJWiaBEPlZ`, 
+        success: function (resposta){
+            $("#dataDaImagem").text(resposta.date);
+            $("#tituloDaImagem").text(resposta.title);
+            $("#imagem-retorno").attr("src", `${resposta.hdurl}`);
+            $("#info").text(resposta.explanation);
         }
-    }
-    requisicao.send();
+    })
 })
+
