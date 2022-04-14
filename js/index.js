47f271e84e5702a6ebc7a643e931377e6c174ce8
Jessica-s-paiva <jessicapaivamvp@gmail.com>
2022-04-14 19:21:57 -0300

    consegui fazer a requisição de videos

--- js/index.js
+++ js/index.js
@@ -1,27 +1,31 @@
 $("#imagem").hide();
 $("p").hide();
+$("#video").hide();
+
 $("#enviar").click(function(event){
     event.preventDefault();
-    
+    const data = $("#data").val();
+    console.log(data);
+    const dataTeste = new Date();
+    const dia = String(dataTeste.getDate()).padStart(2,'0');
+    const mes = String(dataTeste.getMonth() + 1).padStart(2,'0');
+    const ano = dataTeste.getFullYear();
+    const dataAtual = `${ano}-${mes}-${dia}`;
+    const partesData = data.split("-");
     try {
-        const data = $("#data").val();
-        console.log(data);
-        const dataTeste = new Date();
-        const dia = String(dataTeste.getDate()).padStart(2,'0');
-        const mes = String(dataTeste.getMonth() + 1).padStart(2,'0');
-        const ano = dataTeste.getFullYear();
-        const dataAtual = `${ano}-${mes}-${dia}`;
-
-        const partesData = data.split("-");
-
         console.log(partesData);
         $.ajax({
             url: `https://api.nasa.gov/planetary/apod?date=${data}&api_key=w1d9wvAkhHPGhaGl6WqpJYk444B9CYxJWiaBEPlZ`, 
             success: function (resposta){
                 $("#dataDaImagem").text(resposta.date);
                 $("#tituloDaImagem").text(resposta.title);
-                $("#imagem").attr("src", `${resposta.hdurl}`);
-                $("#imagem").show();
+                if(resposta.media_type == 'video'){
+                    $("#video").attr("src", `${resposta.url}`);
+                    $("#video").show();
+                }else if(resposta.media_type == 'image'){
+                    $("#imagem").attr("src", `${resposta.hdurl}`);
+                    $("#imagem").show();
+                }
                 $("p").show();
                 $("#info").text(resposta.explanation);
             }
@@ -29,7 +33,6 @@ $("#enviar").click(function(event){
     } catch (error) {
         if(parseInt(partesData[0]) < 1996) throw new Error("Digite uma data válida");  
         if(parseInt(partesData[1]) <= 06 && (partesData[2]) < 16) throw new Error("Digite uma data válida");
-
     }
 
 })
