c0924ff557d957e0b922c0f2195e89a67d7c03b5
Jessica-s-paiva <jessicapaivamvp@gmail.com>
2022-04-14 16:48:46 -0300

    inserindo tratamento de erro

--- js/index.js
+++ js/index.js
@@ -3,7 +3,15 @@ $("p").hide();
 $("#enviar").click(function(event){
     event.preventDefault();
     const data = $("#data").val();
-    console.log(data);
+    const dataAtual = new Date;
+    dataAtual.getDay();
+    console.log(dataAtual);
+
+    try {
+        
+    } catch (error) {
+        if(data == '1995-06-16') throw new Error("Digite uma data válida");  
+    }
 
     $.ajax({
         url: `https://api.nasa.gov/planetary/apod?date=${data}&api_key=w1d9wvAkhHPGhaGl6WqpJYk444B9CYxJWiaBEPlZ`, 
